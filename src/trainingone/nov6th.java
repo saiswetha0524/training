0625c37d62d5b8012a5550e02bb570abf3d31744
Saiswetha V <saiswetha.v@hcl.com>
2023-11-07 08:01:57 +0530

    finding occurrences by using enhanced forloop and normal forloop

--- src/trainingone/nov6th.java
+++ src/trainingone/nov6th.java
@@ -4,16 +4,16 @@ import java.util.HashMap;
 
 public class nov6th {
 	public static void main(String[] args) {
-		 String s = "saiswetha";
+		 String a = "saiswetha";
 		    
 	      HashMap<Character,Integer> map = new HashMap<>();	      
 
 
-	      for(int i= 0 ; i< s.length() ; i++) {
+	      for(char s:a.toCharArray()) {
 	    	  
 	   	      
 
-	            map.put(s.charAt(i) , map.getOrDefault(s.charAt(i), 0)+1);
+	            map.put(s , map.getOrDefault(s, 0)+1);
 	          
 	         } 
 	      
